1cb7f2149e2f12346861cb187dcbe0a93a0d9352
Bianca <biancamedeiros.bs10@gmail.com>
2020-10-11 11:19:03 -0300

    [update] home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return <View />;
+  return (
+    <View>
+      
+    </View>
+  )
 }
 
 export default Home;

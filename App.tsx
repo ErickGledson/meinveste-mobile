642b328558b0b99b65028cdeb8a6a8e3a7e68696
Bianca <biancamedeiros.bs10@gmail.com>
2020-10-11 11:18:11 -0300

    [add] font config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  useFonts
+} from '@expo-google-fonts/poppins';
+
+import {
+  Ubuntu_400Regular,
+  Ubuntu_500Medium,
+  Ubuntu_700Bold
+} from '@expo-google-fonts/ubuntu';
+
 import AppStack from './src/routes/AppStack';
+import { AppLoading } from 'expo';
 
 export default function App() {
-  return <AppStack/>;
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }

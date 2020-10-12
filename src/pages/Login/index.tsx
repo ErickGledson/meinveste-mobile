3bbbad95d65bab58a94a2594dbd90439accfcdd8
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-12 16:50:08 -0300

    [update] auth login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,71 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { TextInput, useTheme } from 'react-native-paper';
 
 import Header from '../../components/Header';
-import Input from '../../components/Input';
+import api from '../../services/api';
 
 import style from './styles';
 
-const Login: React.FC = () => {
-  const {navigate} = useNavigation();
-  function handleNavigateToNext(){
-    navigate('Home')
+const Login: React.FC = (props) => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const { colors } = useTheme();
+
+  const { navigate } = useNavigation();
+
+  async function SubmitForm() {
+    try {
+      const response = await api.post('api/user/login', {
+        email,
+        password,
+      })
+
+      await AsyncStorage.setItem(
+        'id', response.data.id
+      );
+      await AsyncStorage.setItem(
+        'email', response.data.email
+      );
+      await AsyncStorage.setItem(
+        'name', response.data.name
+      );
+
+      navigate('Home')
+    } catch (error) {
+      console.log(error.response.status)
+    }
   }
+
   return (
     <View style={style.container}>
       <Header menu={false} />
 
       <View style={style.form}>
-        <Input label='Email' type='emailAddress'/>
-        <Input label='Senha' type='password'/>
+        <TextInput
+          {...props}
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={{ backgroundColor: colors.text + "00", marginBottom: 20 }}
+          textContentType='emailAddress'
+          label='Seu email'
+        />
+
+        <TextInput
+          {...props}
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={{ backgroundColor: colors.text + "00", marginBottom: 20 }}
+          textContentType='password'
+          label='Senha'
+          secureTextEntry={true}
+        />
       </View>
 
-      <RectButton style={style.loginButton} onPress={handleNavigateToNext}>
+      <RectButton style={style.loginButton} onPress={SubmitForm}>
         <Text style={style.loginButtonText}>Entrar</Text>
       </RectButton>
 
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
 import React from 'react';
 import { View, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -7,23 +8,70 @@ import Header from '../../components/Header';
 import Input from '../../components/Input';
 
 import style from './styles';
+import { TextInput, useTheme } from 'react-native-paper';
 
-const Register: React.FC = () => {
-  const {navigate} = useNavigation();
-  function handleNavigateToNext(){
-    navigate('Home')
+const Register: React.FC = (props) => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [name, setName] = React.useState('');
+
+  const { colors } = useTheme();
+
+  const { navigate } = useNavigation();
+
+  async function SubmitForm() {
+    try {
+      const response = await api.post('api/user', {
+        user: {
+          email,
+          password,
+          name
+        }
+        
+      })
+      navigate('Login')
+    } catch (error) {
+      console.log(error.response.status)
+    }
   }
+
   return (
     <View style={style.container}>
       <Header menu={false} />
 
       <View style={style.form}>
-        <Input label='Nome Completo' type='password'/>
-        <Input label='Seu email' type='emailAddress'/>
-        <Input label='Senha' type='password'/>
+        <TextInput
+          {...props}
+          value={name}
+          onChangeText={text => setName(text)}
+          style={{ backgroundColor: colors.text + "00", marginBottom: 20 }}
+          textContentType='name'
+          label='Nome Completo'
+        />
+
+        <TextInput
+          {...props}
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={{ backgroundColor: colors.text + "00", marginBottom: 20 }}
+          textContentType='emailAddress'
+          label='Seu email'
+        />
+
+        <TextInput
+          {...props}
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={{ backgroundColor: colors.text + "00", marginBottom: 20 }}
+          textContentType='password'
+          label='Senha'
+          secureTextEntry={true}
+        />
+
       </View>
 
-      <RectButton style={style.loginButton} onPress={handleNavigateToNext}>
+
+      <RectButton style={style.loginButton} onPress={SubmitForm}>
         <Text style={style.loginButtonText}>Cadastrar</Text>
       </RectButton>
 
